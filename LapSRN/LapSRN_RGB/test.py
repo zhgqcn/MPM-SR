eda51b0a0413c233be7d1885de7820341152e996
Zhengguoqing <44897668+zhgqcn@users.noreply.github.com>
2020-11-05 15:01:09 +0800

    Add files via upload

---
+++ LapSRN/LapSRN_RGB/test.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import argparse
+import torch
+from torch.autograd import Variable
+from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Lambda
+
+import numpy as np
+
+# Argument settings
+parser = argparse.ArgumentParser(description='PyTorch LapSRN')
+parser.add_argument('--input', type=str, required=False, default='73-512pix-speed7-ave1.tif', help='input image to use')
+parser.add_argument('--model', type=str, default='LapSRN_model_epoch_100.pth', help='model file to use')
+parser.add_argument('--outputHR2', type=str, default='73_LapSRN_R_epochs100_HR2.tif', help='where to save the output image')
+parser.add_argument('--outputHR4', type=str, default='73_LapSRN_R_epochs100_HR4.tif', help='where to save the output image')
+parser.add_argument('--cuda', action='store_true', help='use cuda')
+
+opt = parser.parse_args()
+
+print(opt)
+
+
+#model = torch.load(opt.model, map_location='cpu')
+model = torch.load(opt.model)
+
+if opt.cuda:
+    model = model.cuda()
+
+transform = Compose(
+    [ToTensor(),
+     # Lambda(lambda x: x.repeat(3,1,1)),
+    ])
+
+img = Image.open(opt.input).convert('RGB')
+# LR_r, _, _ = img.split()
+LR_r = transform(img)
+LR_r = LR_r.unsqueeze(0) #https://www.cnblogs.com/chen-hw/p/11678949.html
+#LR_r = Variable(ToTensor()(LR_r)).view(1, -1, LR_r.size[1], LR_r.size[0])
+if opt.cuda:
+    LR_r = LR_r.cuda()
+
+HR_2_r, HR_4_r = model(LR_r)
+
+HR_2_r = HR_2_r.cpu()
+HR_4_r = HR_4_r.cpu()
+
+out_HR_2_r = HR_2_r.data[0].numpy()
+out_HR_2_r *= 255.0
+out_HR_2_r = out_HR_2_r.clip(0, 255)
+out_HR_2_r = Image.fromarray(np.uint8(out_HR_2_r[0]), mode='L')
+
+out_HR_4_r = HR_4_r.data[0].numpy()
+out_HR_4_r *= 255.0
+out_HR_4_r = out_HR_4_r.clip(0, 255)
+out_HR_4_r = Image.fromarray(np.uint8(out_HR_4_r[0]), mode='L')
+
+out_HR_2_r.save(opt.outputHR2)
+print('output image saved to ', opt.outputHR2)
+out_HR_4_r.save(opt.outputHR4)
+print('output image saved to ', opt.outputHR4)
+
+    
+
---
+++ LapSRN/LapSRN_RGB/train.py
@@ -0,0 +1,141 @@
+from __future__ import print_function
+import argparse
+from math import log10
+from os.path import join
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from model import LasSRN
+from data import get_training_set, get_val_set
+from loss import Loss
+
+import pandas as pd
+
+results_train = {'Avg. Loss': []}
+results_val = {'Avg. Loss': [], 'Avg. PSNR1': [], 'Avg. PSNR2': []}
+
+# Training settings 
+parser = argparse.ArgumentParser(description='PyTorch LapSRN')
+parser.add_argument('--batchSize', type=int, default=1, help='training batch size')
+parser.add_argument('--valBatchSize', type=int, default=1, help='val batch size')
+parser.add_argument('--nEpochs', type=int, default=200, help='number of epochs to train for')
+parser.add_argument('--lr', type=float, default=1e-3, help='Learning Rate. Default=1e-3')
+parser.add_argument('--cuda', action='store_true', help='use cuda?')
+parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+parser.add_argument('--dataset', type=str, default="/content/drive/My Drive/app/DataSet/", help='root of DataSet')
+parser.add_argument('--save_train_csv', type=str, default='./train.csv',help='the root of saving the train results')
+parser.add_argument('--save_val_csv', type=str, default='/val.csv', help='the root of saving the val results')
+parser.add_argument('--save_models', type=str, default='./', help='the root of saving the models')
+opt = parser.parse_args()
+
+print(opt)
+
+
+cuda = opt.cuda
+if cuda and not torch.cuda.is_available():
+    raise Exception("No GPU found, please run without --cuda")
+
+torch.manual_seed(opt.seed)
+if cuda:
+    torch.cuda.manual_seed(opt.seed)
+device = torch.device("cuda" if opt.cuda else "cpu")
+
+print('===> Loading datasets')
+train_set = get_training_set(opt.dataset)
+val_set = get_val_set(opt.dataset)
+training_data_loader = DataLoader(dataset=train_set, batch_size=opt.batchSize, shuffle=True)
+val_data_loader = DataLoader(dataset=val_set, batch_size=opt.valBatchSize, shuffle=False)
+
+
+print('===> Building model')
+model = LasSRN().to(device)
+Loss = Loss()
+criterion = nn.MSELoss()
+if cuda:
+    Loss = Loss.cuda()
+    criterion = criterion.cuda()
+
+
+def train(epoch):
+        epoch_loss = 0
+        for iteration, batch in enumerate(training_data_loader, 1):
+            LR, HR_2_target, HR_4_target = batch[0].to(device), batch[1].to(device), batch[2].to(device)
+
+            optimizer.zero_grad()
+            HR_2, HR_4 = model(LR)
+
+            loss1 = Loss(HR_2, HR_2_target)
+            loss2 = Loss(HR_4, HR_4_target)
+            loss = loss1+loss2
+
+            epoch_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        print("===> Epoch {} Complete: Avg. Loss: {:.4f}".format(epoch, epoch_loss / len(training_data_loader)))
+        results_train['Avg. Loss'].append(float('%.4f'%(epoch_loss / len(training_data_loader))))
+
+        if(epoch % 10 == 0):
+            results_val['Avg. Loss'].append(float('%.4f' % (epoch_loss / len(training_data_loader))))
+
+
+def val():
+    avg_psnr1 = 0
+    avg_psnr2 = 0
+    with torch.no_grad():
+        for batch in val_data_loader:
+            LR, HR_2_target, HR_4_target = batch[0].to(device), batch[1].to(device), batch[2].to(device)
+            HR_2, HR_4 = model(LR)
+            mse1 = criterion(HR_2, HR_2_target)
+            mse2 = criterion(HR_4, HR_4_target)
+            psnr1 = 10 * log10(1 / mse1.item())
+            psnr2 = 10 * log10(1 / mse2.item())
+            avg_psnr1 += psnr1
+            avg_psnr2 += psnr2
+        print("===> Avg. PSNR1: {:.4f} dB".format(avg_psnr1 / len(val_data_loader)))
+        print("===> Avg. PSNR2: {:.4f} dB".format(avg_psnr2 / len(val_data_loader)))
+        results_val['Avg. PSNR1'].append(float('%.2f'%(avg_psnr1 / len(val_data_loader))))
+        results_val['Avg. PSNR2'].append(float('%.2f'%(avg_psnr2 / len(val_data_loader))))
+
+
+def checkpoint(epoch):
+    # model_out_path = "/kaggle/working/LapSRN_model_epoch_r_{}.pth".format(epoch)
+    model_out_path = join(opt.save_models, 'LapSRN3Channels_epoch_{}.pth'.format(epoch))
+    torch.save(model, model_out_path)
+    print("Checkpoint saved to {}".format(model_out_path))
+
+
+lr = opt.lr
+k = 1
+
+for epoch in range(1, opt.nEpochs + 1):
+
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-5)
+
+    train(epoch)
+
+    if epoch % 10 == 0:
+        checkpoint(epoch)
+
+    if epoch % 50 == 0:
+        lr = lr / 2
+
+    if epoch % 10 == 0:
+        val()
+        data_frame_val = pd.DataFrame(
+            data={'Avg. Loss': results_val['Avg. Loss'],
+                  'Avg. PSNR1': results_val['Avg. PSNR1'],
+                  'Avg. PSNR2': results_val['Avg. PSNR2']},
+            index=range(1, k + 1)
+        )
+        data_frame_val.to_csv(opt.save_val_csv, index_label='Epoch')
+        k = k + 1
+
+    data_frame_train = pd.DataFrame(
+        data={'Avg. Loss': results_train['Avg. Loss']},
+        index=range(1, epoch + 1)
+    )
+    data_frame_train.to_csv(opt.save_train_csv, index_label='Epoch')
+
