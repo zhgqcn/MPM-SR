0c3a7d63d2730a1f1ac4cb948245149be99531c9
Zhengguoqing <44897668+zhgqcn@users.noreply.github.com>
2020-11-20 10:27:59 +0800

    Add files via upload

---
+++ LapSRN/LapSRN_HRW_Adagrad/R_Channel/dataset.py
@@ -0,0 +1,49 @@
+import torch.utils.data as data
+from os import listdir
+from os.path import join
+from PIL import Image
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", ".tif"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    r, _, _ = img.split()
+    return r
+
+
+class DatasetFromFolder(data.Dataset):
+    def __init__(self, LR_dir, HR_2_dir, HR_4_dir, LR_transform=None, HR_2_transform=None, HR_4_transform=None):
+        super(DatasetFromFolder, self).__init__()
+        self.image_LRfilenames = [join(LR_dir, x) for x in listdir(LR_dir) if is_image_file(x)]
+        self.image_LRfilenames.sort()
+        # print(self.image_LRfilenames)
+        self.image_HR_2filenames = [join(HR_2_dir, x) for x in listdir(HR_2_dir) if is_image_file(x)]
+        self.image_HR_2filenames.sort()
+        # print(self.image_HR_2filenames)
+        self.image_HR_4filenames = [join(HR_4_dir, x) for x in listdir(HR_4_dir) if is_image_file(x)]
+        self.image_HR_4filenames.sort()
+        # print(self.image_HR_4filenames)
+        self.LR_transform = LR_transform
+        self.HR_2_transform = HR_2_transform
+        self.HR_4_transform = HR_4_transform
+
+    def __getitem__(self, index):
+        LR = load_img(self.image_LRfilenames[index])
+        # print(self.image_LRfilenames[index])
+        HR_2 = load_img(self.image_HR_2filenames[index])
+        # print(self.image_HR_2filenames[index])
+        HR_4 = load_img(self.image_HR_4filenames[index])
+        # print(self.image_HR_4filenames[index])
+        if self.LR_transform:
+            LR = self.LR_transform(LR)
+        if self.HR_2_transform:
+            HR_2 = self.HR_2_transform(HR_2)
+        if self.HR_4_transform:
+            HR_4 = self.HR_4_transform(HR_4)
+        return LR, HR_2, HR_4
+
+    def __len__(self):
+        return len(self.image_LRfilenames)
---
+++ LapSRN/LapSRN_HRW_Adagrad/R_Channel/loss.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn
+from torchvision.models.vgg import vgg16
+
+class Loss(nn.Module):
+    def __init__(self):
+        super(Loss, self).__init__()
+        vgg = vgg16(pretrained=True)
+        loss_network = nn.Sequential(*list(vgg.features)[:31]).eval()  # https://blog.csdn.net/Geek_of_CSDN/article/details/84343971
+        for param in loss_network.parameters():
+            param.requires_grad = False
+        self.loss_network = loss_network
+        self.mse_loss = nn.MSELoss()
+        self.tv_loss = TVLoss()
+
+    def forward(self, out_images, target_images):
+        # Image Loss
+        image_loss = CharbonnierLoss(out_images, target_images)
+        # Perception Loss
+        perception_loss = self.mse_loss(self.loss_network(out_images), self.loss_network(target_images))
+        # TV Loss
+        tv_loss = self.tv_loss(out_images)
+        return image_loss + 1e-8 * perception_loss + 2e-8 * tv_loss
+
+
+def CharbonnierLoss(predict, target):
+    return torch.mean(torch.sqrt(torch.pow((predict-target), 2) + 1e-6)) # epsilon=1e-3
+
+
+class TVLoss(nn.Module):
+    def __init__(self, tv_loss_weight=1):
+        super(TVLoss, self).__init__()
+        self.tv_loss_weight = tv_loss_weight
+
+    def forward(self, x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h = self.tensor_size(x[:, :, 1:, :])
+        count_w = self.tensor_size(x[:, :, :, 1:])
+        h_tv = torch.pow((x[:, :, 1:, :] - x[:, :, :h_x - 1, :]), 2).sum()
+        w_tv = torch.pow((x[:, :, :, 1:] - x[:, :, :, :w_x - 1]), 2).sum()
+        return self.tv_loss_weight * 2 * (h_tv / count_h + w_tv / count_w) / batch_size
+
+    @staticmethod
+    def tensor_size(t):
+        return t.size()[1] * t.size()[2] * t.size()[3]
+
