13dd4e7d073cd66bd70c358000169a30e9e0f67b
Gqq <44897668+zhgqcn@users.noreply.github.com>
2021-01-16 23:06:37 +0800

    Add files via upload

---
+++ Pretreatment/compare_normalization.py
@@ -0,0 +1,122 @@
+'''
+@author: Gqq
+@date: 2020/12/01
+@use: compare normalize and no-normalize
+'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.utils.data as data
+from os import listdir
+from os.path import join
+from PIL import Image
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.transforms import Compose, ToTensor, Lambda, Normalize
+
+
+def Dataset_transform():
+    return Compose([
+        ToTensor(),
+    ])
+
+def Dataset_transform_norm(imgs):
+    if imgs == 'LR':
+        norms = Normalize((0.1815, 0.0378, 0.0000), (0.1599, 0.0896, 1.0000))
+    elif imgs == 'HR_2':
+        norms = Normalize((0.1290, 0.0367, 0.0000), (0.1053, 0.0830, 1.0000))
+    else:
+        norms = Normalize((0.1058, 0.0366, 0.0000), (0.0906, 0.0841, 1.0000))
+    return Compose([
+        ToTensor(),
+        norms
+    ])
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg", ".tif"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    return img
+
+def get_training_set(root):
+    return DatasetFromFolder(root,
+                             transform=Dataset_transform(),
+                             )
+
+def get_training_set_norm(root, imgs):
+        return DatasetFromFolder(root,
+                             transform=Dataset_transform_norm(imgs),
+                             )
+
+
+class DatasetFromFolder(data.Dataset):
+    def __init__(self, root, transform = None):
+        super(DatasetFromFolder, self).__init__()
+        self.image_LRfilenames = [join(root, x) for x in listdir(root) if is_image_file(x)]      
+        self.transform = transform
+
+    def __getitem__(self, index):
+        imgs = load_img(self.image_LRfilenames[index])
+
+        if self.transform:
+            imgs = self.transform(imgs)
+
+        return imgs
+
+    def __len__(self):
+        return len(self.image_LRfilenames)
+
+
+def get_mean_and_std(dataset):
+    '''Compute the mean and std value of dataset.'''
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    print('==> Computing mean and std..')
+    for inputs in dataloader:
+        
+        for i in range(3):
+            mean[i] += inputs[:,i,:,:].mean()  # 取所有数据的相同通道计算
+            std[i] += inputs[:,i,:,:].std()
+    mean.div_(len(dataset))
+    std.div_(len(dataset))
+    return mean, std
+
+print('================================before normalize============================')
+train_set = get_training_set(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_LR')
+mean ,std = get_mean_and_std(train_set)
+print('LR:')
+print(mean, std)
+
+train_set = get_training_set(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_HR_2\\')
+mean ,std = get_mean_and_std(train_set)
+print('HR_2:')
+print(mean, std)
+
+train_set = get_training_set(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_HR_4\\')
+mean ,std = get_mean_and_std(train_set)
+print('HR_4:')
+print(mean, std)
+
+print('================================After normalize============================')
+train_set = get_training_set_norm(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_LR', 'LR')
+mean ,std = get_mean_and_std(train_set)
+print('LR:')
+print(mean, std)
+
+train_set = get_training_set_norm(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_HR_2\\', 'HR_2')
+mean ,std = get_mean_and_std(train_set)
+print('HR_2:')
+print(mean, std)
+
+train_set = get_training_set_norm(r'D:\\A_GraduationProject\\dataset\\DataSet\\train_HR_4\\', 'HR_4')
+mean ,std = get_mean_and_std(train_set)
+print('HR_4:')
+print(mean, std)
+
+
+
